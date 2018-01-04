22e3a2f808e2d6a40c10dc06cfb6f9113f10976e
mkan0141 <kanzume0141@gmail.com>
2018-01-04 23:45:07 +0900

    不正なURLの時にalertするようにした

--- src/js/AtConvert.js
+++ src/js/AtConvert.js
@@ -2,6 +2,10 @@ $(function(){
     $("#convert").click(function(){
        var URL = document.getElementById("beta-name").value;
         splitURL = URL.split('/');
+        if(splitURL[0] != "https:" || splitURL[4] == undefined){
+            alert("コンテストURLを入力してください");
+            return ;
+        }
         for(var i = 0; i < splitURL.length; i++){
             console.log(splitURL[i]);
         }
